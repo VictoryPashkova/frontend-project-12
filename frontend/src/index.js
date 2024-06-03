d8d2e764a610e943539ec7cf63aa084251cdaea6
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-03 18:10:16 +0600

    fixed Providers names

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import store from './redux/store';
 import { I18nextProvider } from 'react-i18next';
 import i18next from './i18n';
-import { Provider, ErrorBoundary } from '@rollbar/react';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 const rollbarConfig = {
   accessToken: process.env.REACT_APP_MY_TOKEN,
@@ -20,20 +20,17 @@ const rollbarConfig = {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-  <I18nextProvider i18n={i18next}>
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
-  </I18nextProvider>
-  </ErrorBoundary>
-  </Provider>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <I18nextProvider i18n={i18next}>
+        <ReduxProvider store={store}>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ReduxProvider>
+      </I18nextProvider>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
