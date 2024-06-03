be03a41ca7ca56a2bfec3a8fcf22ba26991a7fc8
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-03 18:05:41 +0600

    add rollbar/react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,17 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { I18nextProvider } from 'react-i18next';
 import i18next from './i18n';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
+const rollbarConfig = {
+  accessToken: '65a2845baa094776a7c4902509608ccc',
+  environment: 'testenv',
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider config={rollbarConfig}>
+      <ErrorBoundary>
   <I18nextProvider i18n={i18next}>
   <Provider store={store}>
   <React.StrictMode>
@@ -18,6 +25,8 @@ root.render(
   </React.StrictMode>
   </Provider>
   </I18nextProvider>
+  </ErrorBoundary>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
