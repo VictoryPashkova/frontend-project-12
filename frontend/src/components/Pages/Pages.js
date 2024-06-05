e18eae88259e9f8de4b4098dba676d3ab14a8441
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-05 17:54:40 +0600

    fixed ChatPage in Pages.js

--- frontend/src/components/Pages/Pages.js
+++ frontend/src/components/Pages/Pages.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogInPage from './LogInPage/LogIn';
 import RegistrationPage from './RegistrationPage/RegistrationPage';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
-import ChatMainPage from './ChatMainPage/ChatMainPage';
+import ChatPage from './ChatPage/ChatPage';
 
 const AppRouter = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<ChatMainPage />} />
+      <Route path="/" element={<ChatPage />} />
       <Route path="login" element={<LogInPage />} />
       <Route path="registration" element={<RegistrationPage />} />
       <Route path="*" element={<NotFoundPage />} />
