0cd4ed905fcaab275aa3fab450a48ce60d0f7ab3
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-12 13:24:11 +0600

    fixed errors in login func

--- frontend/src/components/Forms/LogInForm.js
+++ frontend/src/components/Forms/LogInForm.js
@@ -28,7 +28,7 @@ const LogInForm = () => {
         navigate('/');
       }
     } catch (e) {
-      setError(t('interface.invalidCredentials'), e);
+      setError(t('interface.invalidCredentials'));
       toast.error(t('interface.invalidCredentials'));
     }
   };
@@ -38,19 +38,6 @@ const LogInForm = () => {
       <ToastContainer />
       <Formik
         initialValues={{ name: '', password: '' }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.name) {
-            errors.name = t('interface.requiredField');
-          } else if (values.name.length < 3 || values.name.length > 20) {
-            errors.name = t('interface.usernameLength');
-          } if (!values.password) {
-            errors.password = t('interface.requiredFiel');
-          } else if (values.password.length < 5) {
-            errors.password = t('interface.passwordLength');
-          }
-          return errors;
-        }}
         onSubmit={(values, { setSubmitting }) => {
           onSubmit(values);
           setSubmitting(false);
@@ -58,8 +45,6 @@ const LogInForm = () => {
       >
         {({
           values,
-          errors,
-          touched,
           handleChange,
           handleBlur,
           handleSubmit,
@@ -75,9 +60,8 @@ const LogInForm = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.name || ''}
-                isInvalid={!!errors.name && touched.name}
+                isInvalid={error}
               />
-              <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>{t('interface.password')}</Form.Label>
@@ -87,9 +71,8 @@ const LogInForm = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.password || ''}
-                isInvalid={!!errors.password && touched.password}
+                isInvalid={error}
               />
-              <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
             </Form.Group>
             {error && <p style={{ color: 'red' }}>{error}</p>}
             <div className="d-grid gap-2">
