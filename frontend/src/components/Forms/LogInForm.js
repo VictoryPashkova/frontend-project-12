11a769ca8fcd5487b866cf4e09f00787c0d2635a
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-12 15:03:42 +0600

    rm ToastContainer in login

--- frontend/src/components/Forms/LogInForm.js
+++ frontend/src/components/Forms/LogInForm.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { ToastContainer, toast } from 'react-toastify';
 import { setCredentials } from '../../redux/reducers/user/registrationSlice';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuth } from '../../context/AuthContext';
@@ -29,61 +28,57 @@ const LogInForm = () => {
       }
     } catch (e) {
       setError(t('interface.invalidCredentials'));
-      toast.error(t('interface.invalidCredentials'));
     }
   };
 
   return (
-    <>
-      <ToastContainer />
-      <Formik
-        initialValues={{ name: '', password: '' }}
-        onSubmit={(values, { setSubmitting }) => {
-          onSubmit(values);
-          setSubmitting(false);
-        }}
-      >
-        {({
-          values,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <h1 style={{ textAlign: 'center' }}>{t('interface.login')}</h1>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>{t('interface.nickname')}</Form.Label>
-              <Form.Control
-                type="text"
-                name="name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.name || ''}
-                isInvalid={error}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>{t('interface.password')}</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password || ''}
-                isInvalid={error}
-              />
-            </Form.Group>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <div className="d-grid gap-2">
-              <Button variant="primary" size="lg" type="submit" disabled={isSubmitting} role="button" name="login">
-                {t('interface.buttons.login')}
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </>
+    <Formik
+      initialValues={{ name: '', password: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        onSubmit(values);
+        setSubmitting(false);
+      }}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <h1 style={{ textAlign: 'center' }}>{t('interface.login')}</h1>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>{t('interface.nickname')}</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.name || ''}
+              isInvalid={error}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>{t('interface.password')}</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password || ''}
+              isInvalid={error}
+            />
+          </Form.Group>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <div className="d-grid gap-2">
+            <Button variant="primary" size="lg" type="submit" disabled={isSubmitting} role="button" name="login">
+              {t('interface.buttons.login')}
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
