ac2b5b7cfa366a0259ef24ff814c42b8fdd6ab34
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-25 19:40:43 +0600

    refactor: remove all console logs

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -5,31 +5,25 @@ const initSocket = (store, sendChannel, addMessage) => {
   const { dispatch } = store;
 
   socket.on('connect', () => {
-    console.log('Socket connected');
   });
 
   socket.on('disconnect', () => {
-    console.log('Socket disconnected');
     socket.connect();
   });
 
   socket.on('connect_error', () => {
-    console.error('Socket connection error');
     socket.connect();
   });
 
   socket.on('reconnect_attempt', () => {
-    console.log('Attempting to reconnect socket...');
     socket.connect();
   });
 
   socket.on('newChannel', (newChannel) => {
-    console.log('Новый канал получен:', newChannel);
     dispatch(sendChannel(newChannel));
   });
 
   socket.on('newMessage', (message) => {
-    console.log('Новое сообщение получено:', message);
     dispatch(addMessage(message));
   });
 
