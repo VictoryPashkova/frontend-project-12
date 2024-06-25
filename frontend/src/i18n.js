f95193a6f8c5d3ac0c3581e46ac847937cf8b69d
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-25 19:48:56 +0600

    refactor: refactor i18n configuration using createInstance

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,16 +3,18 @@ import { initReactI18next } from 'react-i18next';
 import translations from './locales/index.js';
 
 const createI18n = async () => {
-  await i18next
+  const i18nInstance = i18next.createInstance();
+
+  await i18nInstance
     .use(initReactI18next)
     .init({
       resources: translations,
-      fallbackLng: 'ru',
+      lng: 'ru',
       interpolation: {
         escapeValue: false,
       },
     });
-  return i18next;
+  return i18nInstance;
 };
 
 export default createI18n;
